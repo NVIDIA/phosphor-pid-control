88c4aa810a3e9932cc85768c98b0d7fe67e0097a
Rohit PAI <ropai@nvidia.com>
2025-05-15 09:50:24 +0530

    Fix boost version upgrade
    Fix boost upgrade to 1.87
    
    Fixes JIRA https://upstream-sync
    Signed-off-by: Rohit PAI <ropai@nvidia.com>

--- pid/actioncontroller.hpp
+++ pid/actioncontroller.hpp
@@ -4,7 +4,6 @@
 
 #include <boost/asio.hpp>
 #include <boost/asio/io_context.hpp>
-#include <boost/asio/io_service.hpp>
 #include <phosphor-logging/log.hpp>
 #include <sdbusplus/asio/connection.hpp>
 #include <sdbusplus/asio/object_server.hpp>
